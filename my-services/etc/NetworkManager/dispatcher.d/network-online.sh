a5c50a5525459d30748d204d59b6aea1f4c7caa1
Jakobus Schürz <jakob@xundeenergie.at>
2017-06-24 00:54:48 +0200

    xe20170624_005448

--- my-services/etc/NetworkManager/dispatcher.d/network-online.sh
+++ my-services/etc/NetworkManager/dispatcher.d/network-online.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-if /usr/bin/nm-online --timeout 3; then
-  /bin/systemctl start network-online.target
+echo "CMDLINE 1 $1 2 $2"
+
+#UNITS="network-online.target connection-online.target"
+UNITS="connection-online.target"
+
+case $2 in
+    connectivity-change)
+        ;;
+    up)
+        /bin/systemctl start connection-online@${1}.target
+        ;;
+    down)
+        /bin/systemctl stop connection-online@${1}.target
+        ;;
+    *)
+        echo "nwonline cmdline $@"
+        ;;
+esac
+
+if /usr/bin/nm-online --timeout 5; then
+    /bin/systemctl start ${UNITS}
 else
-  /bin/systemctl stop network-online.target
+    /bin/systemctl stop ${UNITS}
 fi
 
 exit 0
